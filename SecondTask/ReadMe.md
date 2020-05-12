aae857b66d395275c9152d82badb9377891ea3b5
unknown <mregydev@gmail.com>
2020-05-12 22:45:53 +0300

    finished second task

---
+++ SecondTask/ReadMe.md
@@ -0,0 +1,6 @@
+- Create a JS function that takes one argument N and returns an array with N numbers.
+
+- In a condition that sum of N numbers is equal 0 and numbers are unique so array doesn't contain any repeated number, the array could contain positive or 0 or negative numbers
+  For example, if we call solution(4), it should return an array with 4 numbers, this array could be [-3,0,1,2] or [3,5,-8,0] but it can't be [1,1,-2,0] as 1 repeated.
+
+- Another example, if we call solution(3), it should return an array with 3 numbers, this array could be [-3,0,3]
---
+++ SecondTask/solution.js
@@ -0,0 +1,30 @@
+/**
+ * @param   N  Number of unique elements which sum should be zero
+ * @return  Array of N unique elements with sum zero
+ */
+function GetNumbers(N) {
+  //return empty array in case N is zero or not defined
+  if (!N) {
+    return [];
+  }
+
+  //return array with one element 0  in case  N is 1
+  if (N == 1) {
+    return [0];
+  }
+
+  //resArr is the result array that will be returned from the function
+  //numIterations is the number of iterations or loop will do
+  let [resArr, numIterations] = N % 2 ? [[0], (N - 1) / 2] : [[], N / 2];
+
+  //iterate and push number and its complement
+  for (let counter = 1; counter <= numIterations; ++counter) {
+    resArr = resArr.concat([counter, -counter]);
+  }
+
+  return resArr;
+}
+
+let res = GetNumbers(15);
+
+console.log(GetNumbers(15));
